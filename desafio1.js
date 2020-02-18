3a6c2e9b0ab97faf42e807b4b17ad2e79d75be51
charlessouza73067 <60630646+charlessouza73067@users.noreply.github.com>
2020-02-17 21:14:58 -0300

    Create desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,62 @@
+1)
+var myvar
+undefined
+
+2)
+myvar = 10
+10
+
+3)
+var soma
+undefined
+
+soma = 15+8
+23
+
+4)
+soma++
+23
+
+5)
+soma
+24
+
+6)
+soma = soma * 3
+72
+
+7)
+soma
+72
+
+8)
+var souninja = true
+undefined
+souninja
+true
+
+9)
+var comida = ['arroz','feijão','ovo']
+undefined
+console.log(comida[1])
+ feijão
+
+10)
+soma == myvar
+false
+myvar <= soma
+true
+
+
+11)
+function divisao(x,y){
+return x /y
+}
+
+divisao(2,4)
+0.5
+divisao(10,2)
+5
+
+
+
